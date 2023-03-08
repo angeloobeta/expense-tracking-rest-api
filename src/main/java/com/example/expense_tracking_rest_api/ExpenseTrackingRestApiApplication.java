20abc0157e2718534fc44de92b1665baf2f14007
Obeta Ifeanyichukwu Malachy <angeloobeta@hotmail.com>
2023-03-08 01:52:29 +0100

    Setup: Configured environment for my Expenses-tracking REST API

---
+++ src/main/java/com/example/expense_tracking_rest_api/ExpenseTrackingRestApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.expense_tracking_rest_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExpenseTrackingRestApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ExpenseTrackingRestApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/expense_tracking_rest_api/ExpenseTrackingRestApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.expense_tracking_rest_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExpenseTrackingRestApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
