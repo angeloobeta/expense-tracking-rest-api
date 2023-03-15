216e7fac7e3573e0ba912d39e87a5a9f625a6296
Obeta Ifeanyichukwu Malachy <angeloobeta@hotmail.com>
2023-03-15 18:35:22 +0100

    Reconfigured the Postgres services, and create "expense_tracker_db" database and added the following tables to the database:     table --> expense_tracker_categories,     table --> expense_tracker_users,     expense_tracker_transaction
    Added the UserRepository, and it's implementation; added the UserService, and it's implementation.
    Implemented the UserRepositoryImpl findById method, getCountByEmail method, createUser method.
    
    Implemented the "register" endpoint in the UserResource class

--- src/main/java/com/example/expense_tracking_rest_api/repositories/UserRepositoryImpl.java
+++ src/main/java/com/example/expense_tracking_rest_api/repositories/UserRepositoryImpl.java
@@ -2,31 +2,90 @@ package com.example.expense_tracking_rest_api.repositories;
 
 import com.example.expense_tracking_rest_api.domain.User;
 import com.example.expense_tracking_rest_api.exception.ExpenseTrackingAuthException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.jdbc.support.GeneratedKeyHolder;
+import org.springframework.jdbc.support.KeyHolder;
 import org.springframework.stereotype.Repository;
+
+import java.sql.PreparedStatement;
+import java.sql.Statement;
 
 @Repository
 public class UserRepositoryImpl implements UserRepository{
 
     // SQL statements for performing operations
-    private static final String SQL_CREATE = "INSERT INTO Expense";
+    private static final String SQL_CREATE = "INSERT INTO EXPENSE_TRACKER(USER_ID, FIRST_NAME, LAST_NAME, EMAIL, " + "PASSWORD) VALUES(NEXTVAL('EXPENSE_TRACKER_USERS_SEQ'), ?, ?, ?, ?)";
+
+    // SQL statement for count by email
+    private  static  final String SQL_COUNT_BY_EMAIL = "SELECT COUNT(*) FROM EXPENSE_TRACKER_USERS WHERE EMAIL = ?";
+
+    // SQL statement for find by user_id
+    private  static  final String SQL_FIND_BY_USER_ID = "SELECT USER_ID, FIRST_NAME, LAST_NAME, EMAIL, PASSWORD" + "FROM EXPENSE_TRACKER_USERS WHERE USER_ID = ?";
+
+    @Autowired
+    JdbcTemplate jdbcTemplate;
 
     @Override
     public Integer createUser(String firstName, String lastName, String email, String password) throws ExpenseTrackingAuthException {
-        return null;
+        try{
+            /*
+            * For performing any db operation we use the Update method of JdbcTemplate and the same goes for insert
+            * but that doesn't return created object or generated key, for that we need a keyholder
+             */
+            KeyHolder keyHolder = new GeneratedKeyHolder();
+
+            jdbcTemplate.update(connection ->{
+                /*
+                * The lambda gets a connection object as parameter. Add a prepared statement
+                */
+
+                PreparedStatement ps = connection.prepareStatement(SQL_CREATE, Statement.RETURN_GENERATED_KEYS);
+                // pass a flag to return generated keys, which will populate keyholder object
+                ps.setString(1, firstName);
+                ps.setString(2,lastName);
+                ps.setString(3, email);
+                ps.setString(4, password);
+
+                // return prepared-statement from this lambda
+                return ps;
+                }, keyHolder);
+            return  (Integer) keyHolder.getKeys().get("USER_ID");
+            /*
+            * As we can have composite primary keys, we get a map from which we are getting specific key for UserID
+             */
+
+        }catch (Exception e){
+            throw new ExpenseTrackingAuthException("Invalid Details. Failed to create account");
+        }
     }
 
     @Override
     public User findByEmailAndPassword(String email, String password) throws ExpenseTrackingAuthException {
-        return null;
+        return  null;
     }
 
     @Override
     public Integer getCountByEmail(String email) {
-        return null;
+        return jdbcTemplate.queryForObject(SQL_COUNT_BY_EMAIL, new Object []{email}, Integer.class);
     }
 
     @Override
     public User findById(Integer userId) {
-        return null;
+        /*
+        *pass userId in params array and the result is a set of user row. We use a row mapper for it
+         */
+
+        return jdbcTemplate.queryForObject(SQL_FIND_BY_USER_ID, new Object []{userId}, userRowMapper );
     }
+    private RowMapper<User> userRowMapper = ((rs, rowNum) -> {
+        return  new User(
+                rs.getInt("USER_ID"),
+                rs.getString("FIRST_NAME"),
+                rs.getString("LAST_NAME"),
+                rs.getString("EMAIL"),
+                rs.getString("PASSWORD")
+        );
+    });
 }
--- src/main/java/com/example/expense_tracking_rest_api/resources/UserResource.java
+++ src/main/java/com/example/expense_tracking_rest_api/resources/UserResource.java
@@ -1,21 +1,38 @@
 package com.example.expense_tracking_rest_api.resources;
 
+import com.example.expense_tracking_rest_api.domain.User;
+import com.example.expense_tracking_rest_api.services.UserServices;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.HashMap;
 import java.util.Map;
 
 @RestController
 @RequestMapping("/api/users")
 public class UserResource {
+
+    @Autowired
+    UserServices userServices;
+
+
+
     @PostMapping("/register")
-    public String registerUser(@RequestBody Map<String, Object> userMap){
+    public ResponseEntity<Map<String, String>> registerUser(@RequestBody Map<String, Object> userMap){
         String firstName = (String) userMap.get("firstName");
         String lastname = (String) userMap.get("lastName");
         String email = (String) userMap.get("email");
         String password = (String) userMap.get("password");
-        return firstName + " " + lastname + " " + email;
+        User user = userServices.registerUser("firstName", "lastName", "email", "password");
+        Map<String, String> map = new HashMap<>();
+        map.put("message", "registered successfully");
+        // Todo: generate jwt token and send from here
+        // Create response entity object, passing in the map and the http status
+        return new ResponseEntity<>(map, HttpStatus.OK);
     }
 }
